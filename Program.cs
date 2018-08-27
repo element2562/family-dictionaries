7e371d9055ef3106f0935ac4979168b3f712d24c
element2562 <element25668@gmail.com>
2018-08-27 11:03:19 -0500

    finished

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){ 
+                {"name", "Krista"},
+                {"age", "42"}
+            });
+            myFamily.Add("brother", new Dictionary<string,string>() {
+                {"name", "Colton"},
+                {"age", "25"}
+            });
+
+            foreach (var item in myFamily)
+            {
+                Console.WriteLine($"{item.Value["name"]} is my {item.Key} and is {item.Value["age"]}");
+            }
+
+        }
+    }
+}
